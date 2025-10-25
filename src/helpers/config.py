a8ab8a8bb261ff3c763e61dc4b876a3de695692b
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-10-25 16:35:36 +0300

    setting database connections

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,6 +9,9 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNCK_SIZE: int
+    
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
         
     class Config:
         env_file = ".env"
---
+++ src/models/db_schemes/data_chunk.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field, validator
+from bson.objectid import ObjectId
+from typing import Optional
+
+class DataChunk(BaseModel):
+    _id: Optional[ObjectId]
+    chunk_text: str = Field(..., min_length=1)
+    chunk_metadata: dict
+    chunk_order: int = Field(..., gt=0)
+    chunk_projet_id: ObjectId    
+    
+    class Config:
+        arbitrary_types_allowed = True

60543dc67a4bcf5b7237a5d2db204ea52f6f86c6
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-11-12 11:27:59 +0200

    add config for llm and vectordb

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,7 +4,6 @@ class Settings(BaseSettings):
     
     APP_NAME: str
     APP_VERSION: str
-    OPENAI_API_KEY: str
     
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
@@ -13,6 +12,25 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DATABASE: str
         
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str
+    OPENAI_API_URL: str
+    COHERE_API_KEY: str
+
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_SIZE: int
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int
+    GENERATION_DEFAULT_MAX_TOKENS: int
+    GENERATION_DEFAULT_TEMPERATURE: float
+    
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_PATH: str
+    VECOTR_DB_DISTANCE_METHOD: str
+
     class Config:
         env_file = ".env"
         

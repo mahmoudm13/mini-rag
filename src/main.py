c3ee777068fcc859f512c73e31b6d3d795117098
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-11-18 11:12:25 +0200

    extend the main the nlp routing

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from contextlib import asynccontextmanager
-
-
+from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,11 +12,39 @@ async def lifespan(app: FastAPI):
     app.state.mongo_conn = AsyncIOMotorClient( settings.MONGODB_URL )
     app.state.db_client = app.state.mongo_conn[settings.MONGODB_DATABASE]
     
+    llm_provider_factory = LLMProviderFactory(settings)
+    vector_db_provider_factory = VectorDBProviderFactory(settings)
+    
+    # generation client
+    app.state.generation_client = llm_provider_factory.create(
+        provider=settings.GENERATION_BACKEND,
+    )
+    app.state.generation_client.set_generation_model(
+        model_id = settings.GENERATION_MODEL_ID,
+    )
+    
+    # embedding client
+    app.state.embedding_client = llm_provider_factory.create(
+        provider=settings.EMBEDDING_BACKEND,
+    )
+    app.state.embedding_client.set_embedding_model(
+        model_id = settings.EMBEDDING_MODEL_ID,
+        embedding_size = settings.EMBEDDING_SIZE,
+    )
+    
+    # vector db client
+    app.state.vector_db_client = vector_db_provider_factory.create(
+        provider=settings.VECTOR_DB_BACKEND,
+    )
+    app.state.vector_db_client.connect()
+    
     yield
     
     app.state.mongo_conn.close()
+    app.state.vector_db_client.disconnect()
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)

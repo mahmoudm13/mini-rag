3bd4e197723bce70e9089b74687a258cf2cba78b
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-11-29 11:12:15 +0200

    fix a bug in the template parser to include the query in the prompt

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -115,7 +115,9 @@ class NLPController(BaseController):
             for i, doc in enumerate(retrived_documents)
         ])
         
-        footer_prompt = self.template_parser.get("rag", "footer_prompt")
+        footer_prompt = self.template_parser.get("rag", "footer_prompt", {
+            "query": query,
+        })
         
         chat_history = [
             self.generation_client.construct_prompt(

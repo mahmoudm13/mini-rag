35a42e0be2286dea3c8c00fade020097531d0194
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-12-07 14:07:41 +0200

    change project_id type to `int`

--- src/controllers/ProjectController.py
+++ src/controllers/ProjectController.py
@@ -6,8 +6,11 @@ class ProjectController(BaseController):
     def __init__(self):
         super().__init__()
         
-    def get_project_path(self, project_id: str):
-        project_dir = os.path.join(self.files_dir, project_id)
+    def get_project_path(self, project_id: int):
+        project_dir = os.path.join(
+            self.files_dir, 
+            str(project_id)
+        )
         os.makedirs(project_dir, exist_ok=True)
         
         return project_dir

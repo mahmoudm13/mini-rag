df83ab9817f851ed71725414b311c020dd4fc3f1
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-12-13 17:01:41 +0200

    FORCE - github actions

--- src/routes/base.py
+++ src/routes/base.py
@@ -14,5 +14,5 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
     return {
         "app_name": app_name,
         "app_version": app_version,
-        "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     }
